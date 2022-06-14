0c7a3b290e3b3d4d5e5ac279219f1c4d86e3b643
clbrannon <chrisl.brannon@gmail.com>
2022-06-13 19:36:34 -0500

    fixed bug in taskform objects

--- src/components/taskForm.js
+++ src/components/taskForm.js
@@ -13,11 +13,13 @@ export const TaskForm = () => {
 
             title: "",
             desc: "",
-            repeat: false,
+            noEnd: false,
             endDate: "",
             assignSelect: ""
 
     })
+
+    {/* TODO!! make desc optional with checkbox */}
 
 
 
@@ -35,15 +37,38 @@ export const TaskForm = () => {
 
     const handleChange = e => {
 
-        if (e.target.id === "repeat") {
+        if (e.target.id === "noEnd") {
             setTask(task => ({...task, [e.target.id]: e.target.checked}))
+            console.log(task)
         }
 
+        else if (e.target.name === "assignSelect") {
+            setTask(task => ({...task, [e.target.name]: e.target.value}))
+        }
+        
         else {
             setTask(task => ({...task, [e.target.id]: e.target.value}))
+            console.log(e.target)
 
         }
 
+    }
+
+    const endDateOption = () => {
+
+        if (!task.noEnd) {
+
+            return <>
+          {/*  <FormControlLabel control={<Checkbox id="repeat" value={task.repeat} onChange={handleChange}/>} label="Repeat"/> */}
+            <TextField id="endDate" label="End Date" value={task.endDate} variant="standard" onChange={handleChange}/>
+            
+            </>
+
+        }
+
+        else { 
+            return 
+        }
     }
 
     const handleSubmit = e => {
@@ -82,20 +107,20 @@ export const TaskForm = () => {
 
         <TextField id="title" label="Title" value={task.title} variant="standard" onChange={handleChange}/>
         <TextField id="desc" label="Description" value={task.desc} variant="standard" onChange={handleChange}/>
-        <FormControlLabel control={<Checkbox id="repeat" value={task.repeat} onChange={handleChange}/>} label="Repeat"/>
-        <TextField id="endDate" label="End Date" value={task.endDate} variant="standard" onChange={handleChange}/>
-        <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-                <InputLabel id="assignLabel">Assign To</InputLabel>
+        <FormControlLabel control={<Checkbox id="noEnd" value={task.noEnd} onChange={handleChange}/>} label="No End Date"/>
+        {endDateOption()}
+        <Box sx={{ minWidth: 120 }} name="D">
+            <FormControl fullWidth name="A">
+                <InputLabel name="B">Assign To</InputLabel>
                 <Select
-                    id="assignSelect"
+                    name="assignSelect"
                     label="Assign To"
                     onChange={handleChange}
                 >       
                     {
                         family.map(
                             (members) => {
-                                return <MenuItem value={members.fullName}>{members.fullName}</MenuItem>
+                                return <MenuItem value={members.fullName} name="E">{members.fullName}</MenuItem>
                             }
                         )
                     }
